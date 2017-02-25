fb4ba128d7f0a6999b4e017696602ae2ae41b74d
Jon Preece <Jon Preece>
2017-02-25 06:53:04 +0000

    Added Flickity, fixed up carousel

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import * as Flickity from 'flickity';
 
 @Component({
     selector: 'app-carousel',
     templateUrl: './carousel.component.html',
     styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
+
+    ngOnInit() {
+        setTimeout(() => {
+            new Flickity('.carousel', {
+                wrapAround: true,
+                autoPlay: 5000,
+                pageDots: false,
+                prevNextButtons: false
+            });
+        });
+    }
 
 }
