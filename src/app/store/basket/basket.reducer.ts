8ff58e76b18ea580d3099e3b785ffd79c8d5d5c2
Jon Preece <Jon Preece>
2017-03-16 20:34:59 +0000

    Quantity WIP

--- src/app/store/basket/basket.reducer.ts
+++ src/app/store/basket/basket.reducer.ts
@@ -2,13 +2,24 @@ import { Action, ActionReducer } from '@ngrx/store';
 import { BASKET_ADD, BASKET_REMOVE } from './basket.actions';
 import { Basket, BasketItem } from '../../app.interfaces';
 
+const increment = (state: Basket = { items: [] }, index: number) => {
+  return Object.assign({}, state, { items: [...state.items] });
+};
+
 export const BasketReducer: ActionReducer<Basket> = (state: Basket = { items: [] }, action: Action): Basket => {
 
   switch (action.type) {
+
     case BASKET_ADD:
+      let index: number = state.items.findIndex((item: BasketItem) => action.payload.id === item.id);
+      if (index > -1) {
+        return increment(state, index);
+      }
       return Object.assign({}, state, { items: [...state.items, action.payload] });
+
     case BASKET_REMOVE:
-      return Object.assign({}, state, { items: state.items.filter((item: BasketItem) => action.payload.id !== item.id)});
+      return Object.assign({}, state, { items: state.items.filter((item: BasketItem) => action.payload.id !== item.id) });
+
     default:
       return state;
   }
